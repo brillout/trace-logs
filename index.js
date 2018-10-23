770d83dfd6e66732a98c9b9b981bc8a3f8bc253b
Romuald Brillout <github.com@brillout.com>
2018-10-23 20:35:05 +0200

    make it work in every circumstance

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 module.exports = traceLogs;
 
-function traceLogs(opts) {
-    ['log', 'warn'].forEach(function(method) {
-        const originalFn = console[method];
-        console[method] = function() {
-            const returnedValue = originalFn.apply(console, arguments);
-            const trace = '^^^ Log comes from: '+get_log_origin(new Error().stack, opts);
-            originalFn.call(console, trace);
-            return returnedValue;
-        };
+function traceLogs({onlyLastCallSite}={}) {
+    /*
+    ["log", "debug", "warn", "info"]
+    .forEach(function(fnName) {
+        add_tracer(console, fnName, false);
     });
-}
+    */
+    add_tracer(process.stdout, 'write', true);
+    add_tracer(process.stderr, 'write', true);
 
-function get_log_origin(stackTrace, {onlyLastCallSite}={}) {
-    let lines = stackTrace.split('\n');
-    lines = lines.slice(2);
-    if( onlyLastCallSite ) {
-        const lastCallSite = lines[1].trim();
-        return lastCallSite;
+    return;
+
+    function add_tracer(obj, fnName, preprendNewLine) {
+      const fn_original = obj[fnName];
+      obj[fnName] = function() {
+        const argumentsCropped = Array.from(arguments).map(arg => {
+          if( arg && arg.constructor===String ) {
+            return arg.slice(0, 1000);
+          }
+        });
+        const returnedValue = fn_original.apply(obj, argumentsCropped);
+        const trace = (
+          (preprendNewLine?'\n':'')+
+          '^^^ Log comes from: '+
+          get_log_origin(new Error().stack)
+        );
+        fn_original.call(obj, trace);
+        return returnedValue;
+      };
     }
-    return '\n'+lines.join('\n');
+
+    function get_log_origin(stackTrace) {
+        let lines = stackTrace.split('\n');
+        lines = lines.slice(2);
+        if( onlyLastCallSite ) {
+            const lastCallSite = lines[1].trim();
+            return lastCallSite;
+        }
+        return '\n'+lines.join('\n');
+  }
 }
